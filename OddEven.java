9a480d735c546ac96db2030b835daa02380db227
gurusaranperiyasamy-byte <gurusaranperiyasamy@gmail.com>
2026-02-07 22:54:22 +0530

    Odd Even Program

---
+++ OddEven.java
@@ -0,0 +1,13 @@
+class OddEven{
+    public static void main (String[]args){
+
+    
+    int num = 17;
+    if(num % 2 == 0){
+        System.out.println("The Number is Even");
+    }
+    else{
+        System.out.println("The Number is Odd");
+    }
+    }
+}
